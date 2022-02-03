0f82080ea63fc8d59f9a2140e6e8135112553344
maxime baillon <baillonmaximebaillon@gmail.com>
2022-02-03 09:13:26 +0100

    ajout de data pour tester

---
+++ checkpoint4/src/data/Data.jsx
@@ -0,0 +1,22 @@
+const data = [
+  {
+    components: "quiz",
+    data: {
+      titre: "Commencer",
+      quiz: [
+        {
+          qestion: "Quel couleur est le ciel ?",
+          choix: ("rouge", "bleu", "vert", "jaune"),
+          reponse: "bleu",
+        },
+        {
+          question: "1 + 1 = ?",
+          choix: ("3", "4", "2", "1"),
+          reponse: "2",
+        },
+      ],
+    },
+  },
+];
+
+export default data;
